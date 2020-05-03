299d7c77a5b4bd0b730c1f2ffa139cebe388aba3
Artem Dubrovskikh <artdubrovskikh@gmail.com>
2020-05-03 14:15:03 +0300

    Просмотр папок работает нормально

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 
-function fold(folder)
-{
-   if (folder.style != undefined) 
-      folder.style.display = "block"
-}
-
 function unfold(folder)
 {
-   
+   folder.childNodes.forEach( child => {
+      if (child.tagName == "UL") 
+         child.style.display = "block"
+   })
 }
 
+function fold(folder)
+{
+    folder.querySelectorAll("ul").forEach(el => {el.style.display = "none"});
+}
 
 document.querySelectorAll(".folders > li > ul").forEach(elem => {elem.style.display = "none"})
 
 document.querySelectorAll("ul.folders > li").forEach(
    elem => elem.addEventListener("click", (event) =>
    {
-      if (event.target.firstChild.nextSibling.style.display == "none")
-         event.target.childNodes.forEach(el => {fold(el)})
-      else
-         event.target.querySelectorAll("ul").forEach(el => {el.style.display = "none"});
+      if (elem == event.target){
+         if (event.target.firstChild.nextSibling.style.display == "none")
+            unfold(event.target)
+         else
+            fold(event.target)
+      }
    })
 );
