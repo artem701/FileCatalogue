1d9cdd7e7f420bb51c1ad499d55aaa7e429286b5
Artem Dubrovskikh <artdubrovskikh@gmail.com>
2020-05-03 13:59:49 +0300

    События по клику на папку работают, однако иногда срабатывают два раза

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 
+function fold(folder)
+{
+   if (folder.style != undefined) 
+      folder.style.display = "block"
+}
+
+function unfold(folder)
+{
+   
+}
+
+
+document.querySelectorAll(".folders > li > ul").forEach(elem => {elem.style.display = "none"})
+
+document.querySelectorAll("ul.folders > li").forEach(
+   elem => elem.addEventListener("click", (event) =>
+   {
+      if (event.target.firstChild.nextSibling.style.display == "none")
+         event.target.childNodes.forEach(el => {fold(el)})
+      else
+         event.target.querySelectorAll("ul").forEach(el => {el.style.display = "none"});
+   })
+);
